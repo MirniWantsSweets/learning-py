9707a8d88641d323a923ebf26bf3af52e1dae753
MirniWantsSweets <vlaaadi@hotmail.com>
2023-08-02 18:33:55 +0200

    rebase

---
+++ ejercicios faciles/ejercicio1.py
@@ -0,0 +1,7 @@
+
+numero = input("Entra un numero")
+
+if int(numero) > 10:
+    print("hola mundo")
+else:
+    print("numero mas peque que 10")
---
+++ sudoku/funciones.py
@@ -0,0 +1,21 @@
+# == Funciones complementarias del programa =========================
+
+def comprobarFila(tablero, fila):
+    "Funcion que comprueba si la fila especificada es valida"
+    ...
+
+def comprobarColumna(tablero, columna):
+    "Funcion que comprueba si la columna especificada es valida"
+    ...
+
+def comprovarCuadrado(tablero, fila, columna):
+    "Funcion que comprueba si el cuadrado especificado es valido"
+    ...
+
+def crearUnTablero():
+    "Funcion que sirve para crear un tablero y devolverlo."
+    ...
+
+def inicializarTablero(tablero):
+    "Funcion que sirve para inicializar un tablero que se le pasa por parametro."
+    ...
