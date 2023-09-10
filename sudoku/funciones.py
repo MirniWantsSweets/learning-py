9fb99544a56db29bc2fce88324858ba4a2158a78
iayala07 <iayala07@iesmigmon.org>
2023-09-11 00:02:41 +0200

    Progreso hasta ahora

--- sudoku/funciones.py
+++ sudoku/funciones.py
@@ -1,17 +1,38 @@
 # == Funciones complementarias del programa =========================
 
-def comprobarFila(tablero, fila):
-    "Funcion que comprueba si la fila especificada es valida"
-    ...
+def comprobarFila(tablero, fila, valor):
+    "Funcion que comprueba si la fila especificada es valida."
+    for x in range(0,9):
+        if tablero[fila][x] == valor:
+            return False
+    return True
 
-def comprobarColumna(tablero, columna):
-    "Funcion que comprueba si la columna especificada es valida"
-    ...
+def comprobarColumna(tablero, columna, valor):
+    "Funcion que comprueba si la columna especificada es valida."
+    for x in range(0,9):
+        if tablero[x][columna] == valor:
+            return False
+    return True    
 
-def comprovarCuadrado(tablero, fila, columna):
-    "Funcion que comprueba si el cuadrado especificado es valido"
-    ...
+def comprobarCuadrado(tablero, fila, columna, valor):
+    "Funcion que comprueba si el cuadrado especificado es valido."
+    sumarAFila = (int(fila/3))*3
+    sumarAColumna = (int(columna/3))*3
 
-def crearYInicializarUnTablero():
+    for x in range(0,3):
+        for y in range(0,3):
+            if tablero[x + sumarAFila][y + sumarAColumna] == valor:
+                return False
+    return True 
+
+def crearYInicializarUnTablero() -> list:
     "Funcion que sirve para crear y inicializar un tablero y devolverlo."
-    ...
+    fila = []
+    for x in range(9):
+        fila.append(-1)
+
+    tablero = []
+    for x in range(9):
+        tablero.append(fila[:])
+
+    return tablero   
--- sudoku/juego.py
+++ sudoku/juego.py
@@ -1,4 +1,4 @@
-import funciones
+from funciones import *
 
 # == Variables globales =============================================
 max_row_size = ...
@@ -8,14 +8,30 @@ max_col_size = ...
 # == Funcion principal ==============================================
 def main():
     # = Crear y inicializar un tablero ====================
-    ...
-    # = Inicializar el tablero ============================
-    ...
+    tablero = crearYInicializarUnTablero()
+    print(tablero)
     # = Preguntar posicion y valor para insertar ==========
-    ...
+    fila = int(input("Que fila quieres elegir?\n"))
+    columna = int(input("Que columna quieres elegir?\n"))
+    valor = int(input("Que valor quieres elegir?\n"))
+
     # = Comprovar si el valor es valido ===================
-    ...
+    #comprobarFila(tablero, 5)
+    if fila < 1 or fila > 9:
+        return 1
+
+    #comprobarColumna(tablero, 5)
+    if columna < 1 or columna > 9:
+        return 1
+
+    #comprovarValor
+    if valor < 1 or valor > 9:
+        return 1
+    
+    comprobarCuadrado(tablero, 4, 4, 8)
+
     # = Si no es valido mostrar mensaje de error ==========
+    if valor
 
 # == Condicion para encender la funcion principal ===================
 if __name__ == "__main__":
