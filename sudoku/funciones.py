2428b713c90c0e27d618ab87d9321b441bed03d7
MirniWantsSweets <vlaaadi@hotmail.com>
2023-09-11 12:40:35 +0200

    Un poco de mejora

--- sudoku/funciones.py
+++ sudoku/funciones.py
@@ -1,38 +1,60 @@
-# == Funciones complementarias del programa =========================
+class Tablero:
+    def __init__(self):
+        "Funcion que sirve para crear y inicializar un tablero y devolverlo."
+        fila = []
+        for x in range(9):
+            fila.append(-1)
 
-def comprobarFila(tablero, fila, valor):
-    "Funcion que comprueba si la fila especificada es valida."
-    for x in range(0,9):
-        if tablero[fila][x] == valor:
-            return False
-    return True
+        tablero = []
+        for x in range(9):
+            tablero.append(fila[:])
+        self.tablero = tablero
+    
+    # == Funciones para comprobar la validez del movimiento del jugador =========================
 
-def comprobarColumna(tablero, columna, valor):
-    "Funcion que comprueba si la columna especificada es valida."
-    for x in range(0,9):
-        if tablero[x][columna] == valor:
-            return False
-    return True    
-
-def comprobarCuadrado(tablero, fila, columna, valor):
-    "Funcion que comprueba si el cuadrado especificado es valido."
-    sumarAFila = (int(fila/3))*3
-    sumarAColumna = (int(columna/3))*3
-
-    for x in range(0,3):
-        for y in range(0,3):
-            if tablero[x + sumarAFila][y + sumarAColumna] == valor:
+    def compruebaFila(self, fila: int, valor: int) -> bool:
+        "Funcion que comprueba si la fila especificada es valida."
+        for x in range(0,9):
+            if self.tablero[fila][x] == valor:
                 return False
-    return True 
+        return True
 
-def crearYInicializarUnTablero() -> list:
-    "Funcion que sirve para crear y inicializar un tablero y devolverlo."
-    fila = []
-    for x in range(9):
-        fila.append(-1)
+    def compruebaColumna(self, columna: int, valor: int) -> bool:
+        "Funcion que comprueba si la columna especificada es valida."
+        for x in range(0,9):
+            if self.tablero[x][columna] == valor:
+                return False
+        return True    
 
-    tablero = []
-    for x in range(9):
-        tablero.append(fila[:])
+    def compruebaCuadrado(self, fila: int, columna: int, valor: int) -> bool:
+        "Funcion que comprueba si el cuadrado especificado es valido."
+        sumarAFila = (int(fila/3))*3
+        sumarAColumna = (int(columna/3))*3
 
-    return tablero   
+        for x in range(0,3):
+            for y in range(0,3):
+                if self.tablero[x + sumarAFila][y + sumarAColumna] == valor:
+                    return False
+        return True
+    
+    # == Funciones complementarias ==============================================================
+
+    def cambiarValor(self, fila, columna, valor) -> None:
+        "Funcion que sirve para actualizar el valor de x fila y x columna"
+        #Falta acabar la linea de abajo
+        self.tablero
+
+    def __str__(self) -> None:
+        "Metodo que imprime un tablero de manera bonita para jugar"
+        res = "--------------------------------------------------\n"
+        res += "--------------------------------------------------\n"
+        need_double_line = 1
+        for linea in self.tablero:
+            res = res + "|| " + str(linea[0]) + " | " + str(linea[1]) + " | " + str(linea[2]) + " || " + str(linea[3]) + " | " + str(linea[4]) + " | " + str(linea[5]) + " || " + str(linea[6]) + " | " + str(linea[7]) + " | " + str(linea[8]) + " ||\n"
+            res = res + "--------------------------------------------------\n"
+            if need_double_line == 3:
+                res = res + "--------------------------------------------------\n"
+                need_double_line = 1
+            else:
+                need_double_line += 1
+        return res
--- sudoku/juego.py
+++ sudoku/juego.py
@@ -8,31 +8,59 @@ max_col_size = ...
 # == Funcion principal ==============================================
 def main():
     # = Crear y inicializar un tablero ====================
-    tablero = crearYInicializarUnTablero()
+    tablero = Tablero()
     print(tablero)
     # = Preguntar posicion y valor para insertar ==========
-    fila = int(input("Que fila quieres elegir?\n"))
-    columna = int(input("Que columna quieres elegir?\n"))
-    valor = int(input("Que valor quieres elegir?\n"))
-
-    # = Comprovar si el valor es valido ===================
-    #comprobarFila(tablero, 5)
-    if fila < 1 or fila > 9:
-        return 1
-
-    #comprobarColumna(tablero, 5)
-    if columna < 1 or columna > 9:
-        return 1
-
-    #comprovarValor
-    if valor < 1 or valor > 9:
-        return 1
     
-    comprobarCuadrado(tablero, 4, 4, 8)
+    # == Preguntamos la fila hasta obtener un valor de fila valido
+    while fila < 1 or fila > 9:
+        fila = int(input("Que fila quieres elegir?\n"))
+    
+    # == Preguntamos la columna hasta obtener un valor de columna valido
+    while columna < 1 or columna > 9:
+        columna = int(input("Que columna quieres elegir?\n"))
+    
+    # == Preguntamos el valor hasta obtener un valor valido
+    while valor < 1 or valor > 9:
+        valor = int(input("Que valor quieres elegir?\n"))
+
+    # = Llegados a este punto tenemos un tablero creado y inicializado
+    # = y las variables fila, columna y valor
+
+    # = Comprobamos que los valores introducidos se puedan meter en la tabla
+    # == Comprobamos si se pueden meter en la fila
+
+    # == Comprobamos si se pueden meter en la columna
+
+    # == Comprobamos si se pueden meter en el cuadrado
+    
+    # Tienes que utilizar las siguientes funciones:
+    #tablero.compruebaFila()
+    #tablero.compruebaColumna()
+    #tablero.compruebaCuadrado()
 
     # = Si no es valido mostrar mensaje de error ==========
-    if valor
+
+    # = Si es valido cambiar el valor de la posicion por el valor obtenido
 
 # == Condicion para encender la funcion principal ===================
 if __name__ == "__main__":
     main()
+
+
+#####################################################################
+# Codigo viejo
+#####################################################################
+
+# # = Comprobar si el valor es valido ===================
+# #tablero.compruebaFila(tablero, 5)
+# if fila < 1 or fila > 9:
+#     return 1
+
+# #tablero.compruebaColumna(tablero, 5)
+# if columna < 1 or columna > 9:
+#     return 1
+
+# #comprovarValor
+# if valor < 1 or valor > 9:
+#     return 1
